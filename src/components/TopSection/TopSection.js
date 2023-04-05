66a7f322d0d89abcf42255d543f1d017788093c4
Nick C <niemal@users.noreply.github.com>
2023-04-05 17:23:49 +0300

    Main animation performance polish

--- src/components/TopSection/TopSection.js
+++ src/components/TopSection/TopSection.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Button from "../Button";
 import ClickableWrapper from "../ClickableWrapper";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
 import { QUERIES } from "../constants";
@@ -70,6 +70,8 @@ const Title = styled.h1`
   line-height: ${46 / 16}rem;
   color: var(--color-white);
 
+  opacity: ${(p) => (p.showText ? "1" : "0")};
+
   @media ${QUERIES.phoneAndSmaller} {
     font-size: ${24 / 16}rem;
     line-height: ${36 / 16}rem;
@@ -84,6 +86,8 @@ const TextDesc = styled.p`
   font-size: ${16 / 16}rem;
   line-height: ${24 / 16}rem;
   max-width: 484px;
+
+  opacity: ${(p) => (p.showText ? "1" : "0")};
 
   @media ${QUERIES.phoneAndSmaller} {
     font-size: ${14 / 16}rem;
@@ -111,7 +115,7 @@ const spinIn = {
   animate: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 1, ease: "easeInOut" },
+    transition: { duration: 1, ease: "easeIn" },
   },
 };
 
@@ -139,14 +143,22 @@ const iconsAnimation = {
     x: custom.x,
     y: custom.y,
     opacity: 1,
-    transition: { duration: 0.5, ease: "easeOut" },
+    transition: { duration: 1.6, ease: "easeOut" },
   }),
-  center: {
+  float: (custom) => ({
+    x: custom.x,
+    y: [custom.y - 5, custom.y + 5],
+    transition: {
+      y: { duration: 7, yoyo: Infinity, ease: "easeInOut" },
+    },
+  }),
+  center: (custom) => ({
     x: 0,
     y: 0,
     opacity: 0,
-    transition: { duration: 0.2, ease: "easeIn" },
-  },
+    rotate: custom.y > 0 ? 90 : -90,
+    transition: { duration: 0.38, ease: "easeIn" },
+  }),
 };
 
 const getRandomPosition = () => {
@@ -157,40 +169,49 @@ const getRandomPosition = () => {
   return { x, y };
 };
 
-function TopSection() {
-  const [iconAnimation, setIconAnimation] = useState("initial");
+const FileIconWithAnimation = ({ index, src, alt, ...props }) => {
+  const randomPosition = getRandomPosition();
+  const controls = useAnimation();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIconAnimation("spread");
-    }, 1000); // Trigger animation after IllustrationContainer animation has ended
-
-    return () => {
-      clearTimeout(timer);
+    const spreadAnimation = async () => {
+      await controls.start("spread");
+      await controls.start("float");
+      await controls.start("center");
     };
-  }, []);
 
-  const fileIcons = Array.from({ length: 20 }, (_, index) => {
-    const randomPosition = getRandomPosition();
-    return (
-      <FileIcon
-        key={index}
-        src={"/frontendmentor_18/file-solid.svg"}
-        alt={`file icon ${index}`}
-        custom={randomPosition}
-        variants={iconsAnimation}
-        initial="initial"
-        animate={iconAnimation}
-        onAnimationComplete={(definition) => {
-          if (definition === "spread") {
-            setTimeout(() => {
-              setIconAnimation("center");
-            }, 1500); // Pause for 1 second before pulling icons back to the center
-          }
-        }}
-      />
-    );
-  });
+    spreadAnimation();
+  }, [controls]);
+
+  return (
+    <FileIcon
+      key={index}
+      src={src}
+      alt={alt}
+      custom={randomPosition}
+      variants={iconsAnimation}
+      initial="initial"
+      animate={controls}
+      {...props}
+    />
+  );
+};
+
+function TopSection() {
+  const [showFileIcons, setShowFileIcons] = useState(false);
+  const [showTitle, setShowTitle] = useState(false);
+  const [showDesc, setShowDesc] = useState(false);
+
+  const fileIcons = Array.from({ length: 20 }, (_, index) => (
+    <FileIconWithAnimation
+      key={index}
+      src={"/frontendmentor_18/file-solid.svg"}
+      alt={`file icon ${index}`}
+      onAnimationComplete={() => {
+        setShowTitle(true);
+      }}
+    />
+  ));
 
   return (
     <AnimatePresence>
@@ -204,42 +225,56 @@ function TopSection() {
           initial="initial"
           animate="animate"
           variants={spinIn}
+          onAnimationComplete={() => {
+            setShowFileIcons(true);
+          }}
         >
           <Image
             src={"/frontendmentor_18/illustration-intro.png"}
             alt={"illustration intro image"}
           />
-          {fileIcons}
+          {showFileIcons && fileIcons}
         </IllustrationContainer>
         <ContentWrapper>
           <motion.div>
-            <Title>
-              {Array.from(
-                "All your files in one secure location, accessible anywhere."
-              ).map((letter, index) => (
-                <motion.span
-                  key={index}
-                  custom={index}
-                  variants={letterAnimation}
-                >
-                  {letter}
-                </motion.span>
-              ))}
+            <Title showText={showTitle}>
+              {showTitle
+                ? Array.from(
+                    "All your files in one secure location, accessible anywhere."
+                  ).map((letter, index) => (
+                    <motion.span
+                      key={index}
+                      custom={index}
+                      variants={letterAnimation}
+                      onAnimationComplete={
+                        index === 58
+                          ? () => {
+                              setShowDesc(true);
+                            }
+                          : null
+                      }
+                    >
+                      {letter}
+                    </motion.span>
+                  ))
+                : "All your files in one secure location, accessible anywhere."}
             </Title>
           </motion.div>
           <motion.div>
-            <TextDesc>
-              {Array.from(
-                "Flylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers."
-              ).map((letter, index) => (
-                <motion.span
-                  key={`desc-${index}`}
-                  custom={index}
-                  variants={letterAnimationFast}
-                >
-                  {letter}
-                </motion.span>
-              ))}
+            <TextDesc showText={showDesc}>
+              {showDesc
+                ? Array.from(
+                    "Flylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers."
+                  ).map((letter, index) => (
+                    <motion.span
+                      key={`desc-${index}`}
+                      custom={index}
+                      variants={letterAnimationFast}
+                    >
+                      {letter}
+                    </motion.span>
+                  ))
+                : "Flylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers."}
             </TextDesc>
           </motion.div>
 
